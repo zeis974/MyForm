715edf5a81507d5f8fe5dc09bf176b40bad8c1cf
zeis974 <zeis.mcpe974@gmail.com>
2022-05-04 20:44:21 +0400

    fix: navbar scroll on menu
    Now, navbar not scroll when menu is open

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,14 +1,24 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const Context = createContext();
 
 export function AppProvider({ children }) {
  const [handleGesture, setHandleGesture] = useState();
+ const [scroll, setScroll] = useState();
+
+ /* Detect scroll state */
+ useEffect(() => {
+  if (scroll === false) document.body.className = "no-scroll";
+  else if (scroll === true) document.body.classList.remove("no-scroll");
+ }, [scroll]);
+
  return (
   <Context.Provider
    value={{
     setHandleGesture,
     handleGesture,
+    setScroll,
+    scroll,
    }}
   >
    {children}
