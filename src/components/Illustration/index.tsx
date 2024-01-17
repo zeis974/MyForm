43b6d8eba8a4dc86a3fc0c17a842a0b113e5da19
zeis974 <38920781+zeis974@users.noreply.github.com>
2024-01-17 23:43:33 +0400

    ref: move illustration
    Move illustration files into React component

---
+++ src/components/Illustration/index.tsx
@@ -0,0 +1,13 @@
+import SearchIllustration from "./SearchIllustration"
+import MathsIllustration from "./MathsIllustration"
+import PhysiqueIllustration from "./PhysiqueIllustration"
+import I2dIllustration from "./I2dIllustration"
+import NotFoundIllustration from "./NotFoundIllustration"
+
+export {
+  SearchIllustration,
+  MathsIllustration,
+  PhysiqueIllustration,
+  I2dIllustration,
+  NotFoundIllustration,
+}
