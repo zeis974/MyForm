119bc072c615109076f12c03893c7cb68237c983
zeis974 <38920781+zeis974@users.noreply.github.com>
2024-01-30 22:42:27 +0400

    ref: not-found page render
    - With the change to CSS-in-JS I take the opportunity to optimize the code, the not-found page encapsulated in a client component causes a display delay depending on the path `/fiches/*` of around a few seconds on rendering the redirect button.
    
    - Removed unnecessary use of rendering to know the current path.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,51 +1,31 @@
-"use client";
+import { Container, Content } from "@/styles/NotFound.styled"
 
-import style from "@/styles/NotFound.module.css";
+import type { Metadata } from "next"
 
-import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
-import { Metadata } from "next";
-
-import FancyButton from "@/components/FancyButton";
+import { RedirectSubjectButton, FancyButton } from "@/components/Buttons"
+import { NotFoundIllustration } from "@/components/Illustration"
 
 export const metadata: Metadata = {
- title: "404 - Page Introuvable | MyForm",
-};
+  title: "404 - Page Introuvable",
+}
 
 export default function NotFound() {
- const pathname = usePathname();
- const [form, setForm] = useState("");
-
- useEffect(() => {
-  let formArr = ["maths", "physique-chimie", "i2d"];
-
-  if (pathname?.includes("/fiches")) {
-   formArr.forEach((form) => {
-    if (pathname.includes(form)) {
-     setForm(form);
-    }
-   });
-  }
- }, [pathname]);
-
- return (
-  <div className={style.container}>
-   <div className={style.container__text}>
-    <span>TU ES PERDU ?</span>
-    <p>
-     On dirait que tu t&apos;es égaré. Tu sais ce qui peut aider quand on est
-     perdu ? Une bobine de laine. Tiens, assieds-toi, on travaille dur ici pour
-     apprendre. Ah, tu as besoin d&apos;un peu de lecture ? Ceci
-     t&apos;intéresseras peut-être :
-    </p>
-    <div className={style.container__btn}>
-     <FancyButton name="Page d'accueil" path="/" />
-     {form && (
-      <FancyButton name={`Fiche de ${form}`} path={`/fiches/${form}`} />
-     )}
-    </div>
-   </div>
-   <div className={style.container__illustration}></div>
-  </div>
- );
+  return (
+    <Container>
+      <Content>
+        <h2>TU ES PERDU ?</h2>
+        <p>
+          On dirait que tu t&apos;es égaré. Tu sais ce qui peut aider quand on
+          est perdu ? Une bobine de laine. Tiens, assieds-toi, on travaille dur
+          ici pour apprendre. Ah, tu as besoin d&apos;un peu de lecture ? Ceci
+          t&apos;intéresseras peut-être :
+        </p>
+        <div>
+          <FancyButton name="Page d'accueil" path="/" />
+          <RedirectSubjectButton />
+        </div>
+      </Content>
+      <NotFoundIllustration />
+    </Container>
+  )
 }
