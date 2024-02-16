39a0c5c949e96f7f4dd49a3f05c74d52ee10d5a4
zeis974 <38920781+zeis974@users.noreply.github.com>
2024-02-16 22:28:27 +0400

    format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,35 @@
-import "@/styles/globals.css";
+import "@/styles/globals.css"
 
-import { Metadata } from "next";
-import localFont from "next/font/local";
-import { PropsWithChildren } from "react";
+import type { Metadata } from "next"
+import type { PropsWithChildren } from "react"
 
-import Navbar from "@/components/Navbar";
+import localFont from "next/font/local"
+import { KumaRegistry } from "@kuma-ui/next-plugin/registry"
+
+import Navbar from "@/components/Navbar"
 
 export const metadata: Metadata = {
- title: {
-  template: "%s | MyForm",
-  default: "MyForm",
- },
-};
+  title: {
+    template: "%s | MyForm",
+    default: "MyForm",
+  },
+}
 
 const Rowdies = localFont({
- src: "../../public/assets/fonts/RowdiesRegular.woff2",
- display: "swap",
- variable: "--rowdies",
-});
+  src: "../../public/assets/fonts/RowdiesRegular.woff2",
+  display: "swap",
+  variable: "--rowdies",
+})
 
 export default function RootLayout({ children }: PropsWithChildren) {
- return (
-  <html lang="fr" className={`${Rowdies.variable}`}>
-   <body>
-    <Navbar />
-    {children}
-   </body>
-  </html>
- );
+  return (
+    <html lang="fr" className={`${Rowdies.variable}`}>
+      <body>
+        <KumaRegistry>
+          <Navbar />
+          {children}
+        </KumaRegistry>
+      </body>
+    </html>
+  )
 }
