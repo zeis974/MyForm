5b89efa8973aefa9983a1a7b556078cf96c814e3
zeis974 <38920781+zeis974@users.noreply.github.com>
2024-03-15 18:43:27 +0400

    Implement `isSpeciality` checkbox

--- src/components/FormSearch/index.tsx
+++ src/components/FormSearch/index.tsx
@@ -6,7 +6,8 @@ import type { ChangeEvent } from "react"
 import { useFormStore } from "@/stores/FormStores"
 
 export default function FormSearch() {
-  const setSearch = useFormStore((state) => state.setSearch)
+  const setSearch = useFormStore((s) => s.setSearch)
+  const { setIsSpeciality, isSpeciality } = useFormStore()
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
@@ -16,7 +17,8 @@ export default function FormSearch() {
     <Container>
       <Checkbox>
         <label htmlFor="spé">Spécialité</label>
-        <input type="checkbox" id="spé" name="spé" />
+        <input type="checkbox" id="spé" name="spé" onClick={() => setIsSpeciality(!isSpeciality)
+        } />
       </Checkbox>
       <Line />
       <Input
--- src/functions/formsFilters.tsx
+++ src/functions/formsFilters.tsx
@@ -6,15 +6,18 @@ import { useFormStore } from "@/stores/FormStores"
 import FormCard from "@/components/FormCard"
 
 export default function FormsFilters({ posts, subject }: FormPropsType) {
-  const search = useFormStore((state) => state.search)
+  const search = useFormStore((s) => s.search)
+  const isSpeciality = useFormStore((s) => s.isSpeciality)
 
   const filteredForms = posts.filter((form) => {
     const isMatchingField = form.title
       .toLowerCase()
       .includes(search.toLowerCase())
 
+    const isMatchingSpeciality = isSpeciality ? form.tags.includes("spécialité") : true
     const isMatchingSubject = form.subject.toLowerCase() === subject
-    return isMatchingField && isMatchingSubject
+
+    return isMatchingField && isMatchingSubject && isMatchingSpeciality
   })
 
   return filteredForms.map((form) => <FormCard key={form.slug} {...form} />)
